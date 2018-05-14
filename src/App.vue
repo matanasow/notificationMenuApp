32d238eab03bb4a0d1286cd1d41c9094e654317d
Milen Atanasov <milen.atanasov@amexis.net>
2018-05-14 20:04:57 +0300

    added animation and styles

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,64 @@
 <template>
   <div id="app">
-    <hello v-bind:notifications="notifications">
-    </hello>
+    <notificationMenu v-bind:notifications="notifications"  
+    v-bind:editedNotificationId="editedNotificationId"
+    v-bind:deletedNotificationId="deletedNotificationId">
+    </notificationMenu>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import NotificationMenu from './components/NotificationMenu'
 
 export default {
   name: 'App',
   components:{
-     'Hello': HelloWorld,
+     'notificationMenu': NotificationMenu,
 
   },
   data(){
 return {
-  notifications: []
+  notifications: [],
+  editedNotificationId: 0,
+  deletedNotificationId: 0,
+  notificationForPush:
+      {
+    id: 12,
+    type: 'text',
+    title: 'Test notification',
+    text: 'Test text notification',
+    expires: 5
+  },
+  secondNotificationForPush:
+  {
+    id: 6,
+    type: 'bonus',
+    title: 'You win a bonus!',
+    requirement: 'Deposit $50 to win',
+    expires: 3600
+  }
 }
 },
-mounted: function(){
+mounted(){
   setTimeout(() => {
-    this.firstNotifications();
+    this.firstNotifications()
     setTimeout(() => {
-        this.secondNotifications();
+        this.pushNotification(this.notificationForPush)
         setTimeout(() => {
-          this.thirdNotifications();
+          this.pushNotification(this.secondNotificationForPush)
         },2000)  
     },2000)
   },2000)
- 
+  setTimeout(() => {
+    this.editNotification()
+    setTimeout(() => {
+      this.deleteNotification()
+    }, 10000)
+  },10000)
 },
 methods: {
-  firstNotifications: function(){
-    console.log("1")
-    this.notifications = [
-
+  firstNotifications(){
+    this.notifications =[
   {
     id: 1321,
     type: 'text',
@@ -66,59 +89,27 @@ methods: {
   }
 ];
   },
-  secondNotifications: function(){
-    console.log("2")
-    this.notifications = [
-
-  {
-    id: 1321,
-    type: 'text',
-    title: 'Test notification',
-    text: 'Test text notification',
-    expires: 3600
-  },
-  {
-    id: 4322,
-    type: 'bonus',
-    title: 'You win a bonus!',
-    requirement: 'Deposit $50 to win',
-    expires: 3600
-  }
-];
+  pushNotification(notification){
+    this.notifications.push(notification)
   },
-  thirdNotifications: function(){
-    console.log("3")
-    this.notifications = [
-
-  {
-    id: 1321,
+  deleteNotification(){
+    let notificationForDeleteId = 4322
+    this.deletedNotificationId = notificationForDeleteId
+var foundNotificationForDeleteIndex = this.notifications.findIndex(x => x.id == notificationForDeleteId);
+    this.notifications.splice(foundNotificationForDeleteIndex,1)
+  },
+  editNotification(){
+    let notificationForEditId = 1321
+    this.editedNotificationId = notificationForEditId
+      var editedNotification = {    
+          id: notificationForEditId,
     type: 'text',
-    title: 'Test notification',
-    text: 'Test text notification',
-    expires: 3600
-  },
-  {
-    id: 4322,
-    type: 'bonus',
-    title: 'You win a bonus!',
-    requirement: 'Deposit $50 to win',
-    expires: 3600
-  },
-  {
-    id: 5453,
-    type: 'Promotion',
-    image: 'http://www.absolat.com/images/promotion-in-marketing.jpg',
-    title: '%30 off on sports betting',
-    link: 'https://www.google.com/',
-  },
-  {
-    id: 5236,
-    type: 'text',
-    title: 'Test notification',
-    text: 'Test text notification',
-    expires: 5
-  }
-];
+    title: 'Modified test notification',
+    text: 'Modified test text notification',
+    expires: 360
+}
+var foundNotificationForEditIndex = this.notifications.findIndex(x => x.id == editedNotification.id);
+this.$set(this.notifications, foundNotificationForEditIndex, editedNotification)
   }
 }
 }
@@ -132,5 +123,10 @@ methods: {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  background-image: url('https://www.tutorialspoint.com/opencv/images/gaussian_blur.jpg');
+  background-repeat: no-repeat;
+  background-size: 100%;
 }
 </style>
